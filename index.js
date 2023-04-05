64d922f9b01f0e40ad2fc2f36bca851257f25713
Jess Leyva <jessica.leyvat@yahoo.com>
2023-04-04 19:14:27 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(array){
+    let winningYear = array.find(record => {return record.result === "W"})
+    if (winningYear){
+      return winningYear.year
+    }
+}
